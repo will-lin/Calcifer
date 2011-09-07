fc4ddb639df21d59b045157a13e1e5e63cad5beb
Will Lin <exodus.ejb@live.com>
2011-09-07 21:04:09 +0800

    Signed-off-by: Will Lin <exodus.ejb@live.com>

--- TestCloud/src/test/TestServlet.java
+++ TestCloud/src/test/TestServlet.java
@@ -1,6 +1,8 @@
 package test;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -26,6 +28,8 @@ public class TestServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		PrintWriter out = response.getWriter();
+		
 	}
 
 	/**
